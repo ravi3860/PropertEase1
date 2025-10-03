ba2bafc6f3b7aeb298768061688c568550155428
ravi3860 <167406790+ravi3860@users.noreply.github.com>
2025-10-03 22:51:38 +0530

    file path change

--- config/purchases.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'min_deposit_percent' => 5,      // minimum allowed percent
-    'max_deposit_percent' => 30,     // maximum allowed percent
-    'deposit_cap' => 10000000,       // max deposit amount (LKR 10,000,000)
-    'reservation_days' => 7,         // days to reserve after deposit paid
-    'platform_fee_percent' => 2.5,   // optional platform fee
-];
--- storage/framework/views/6ba0b1efc3c78dce900e49ccfc6de7e0.php
+++
@@ -1,9 +0,0 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    
-
-    <div class="">
-        <?php echo e($slot); ?>
-
-    </div>
-</div>
-<?php /**PATH C:\Users\banda\OneDrive\Desktop\PropertEase\resources\views/components/authentication-card.blade.php ENDPATH**/ ?>
